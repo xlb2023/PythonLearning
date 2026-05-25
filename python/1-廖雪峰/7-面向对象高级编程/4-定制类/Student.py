4bef1c74e138d3fccc468b4382a332d5d79fec0b
邢伟 <xingwei@xingweideMacBook-Air-916.local>
2026-05-25 23:16:02 +0800

    add __getitem__ slice

--- python/1-廖雪峰/7-面向对象高级编程/4-定制类/Student.py
+++ python/1-廖雪峰/7-面向对象高级编程/4-定制类/Student.py
@@ -24,6 +24,38 @@ class Fib(object):
 for n in Fib():
   print(n)
   #__getitem__  是一个特殊方法，用于定义当使用索引访问对象时的行为。
-  
+class Fib(object):
+  def __getitem__(self, n):
+    a, b = 1, 1
+    for x in range(n):
+      a, b = b, a + b
+    return a
+f = Fib()
+print(f[0]) # 1
+print(f[1]) # 1
+print(f[2]) # 2
+print(f[3]) # 3
+ # 使用__getitem__ 可以实现切片（slice）操作
+class Fib(object):
+  def __getitem__(self, n):
+    if isinstance(n, int): # n是索引
+      a, b = 1, 1
+      for x in range(n):
+        a, b = b, a + b
+      return a
+    if isinstance(n, slice): # n是切片
+        start = n.start
+        stop = n.stop
+        if start is None:
+          start = 0
+        a, b = 1, 1
+        L = []
+        for x in range(stop):
+          if x >= start:
+            L.append(a)
+          a, b = b, a + b
+        return L
+f = Fib()
+print(f[0:5]) # [1, 1, 2, 3, 5]
 
 
